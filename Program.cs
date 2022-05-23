39f43ca3b0e9da2ff36dcc7d80af17a319a72e2b
Antonio Junior <antoniocarlosjuninhojuca@gmail.com>
2022-05-23 18:29:12 -0300

    Exercicio-3

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿double venda = 0;
+double comissao = 5;
+double resultado = 0;
+
+Console.WriteLine("Qual valor você arrecadou?");
+venda = Convert.ToInt32(Console.ReadLine());
+resultado = (venda * comissao /100);
+
+Console.WriteLine($"o valor recebido vai ser {resultado}");
+
